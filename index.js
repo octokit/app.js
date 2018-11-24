63bd6e374b7563cbb3d9e19cbac035ac163316cb
Gregor <gregor@martynus.net>
2018-11-23 22:27:19 -0800

    feat: caching installation tokens

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 module.exports = App
 
-const getSignedJsonWebToken = require('./lib/get-signed-json-web-token')
+const getCache = require('./lib/get-cache')
 const getInstallationAccesToken = require('./lib/get-installation-access-token')
+const getSignedJsonWebToken = require('./lib/get-signed-json-web-token')
 
-function App ({ id, privateKey }) {
+function App ({ id, privateKey, cache }) {
   const state = {
     id,
-    privateKey
+    privateKey,
+    cache: cache || getCache()
   }
   const api = {
     getSignedJsonWebToken: getSignedJsonWebToken.bind(null, state),
