6dbc4c068c18b28c8db53ca267131ecae7b1dadf
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2019-01-29 00:34:57 -0500

    feat(typescript): Add typings for the module (#16)

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+import LRU = require("lru-cache");
+
+declare interface AppOptions {
+  id: string
+  privateKey: string
+  baseUrl?: string
+  cache?: LRU.Cache<string, string>
+}
+
+declare interface getInstallationAccessTokenOptions {
+  installation_id: number
+}
+
+// Not really a class, but it is how they say it should be used in the readme.
+// In TypeScript, you cannot use the `new` keyword on functions (excluding old-style classes using functions and prototype), only on classes
+declare class App {
+  constructor(options: AppOptions)
+  public getSignedJsonWebToken(): string
+  public getInstallationAccessToken(options?: getInstallationAccessTokenOptions): Promise<string>
+}
+
+export = App;
