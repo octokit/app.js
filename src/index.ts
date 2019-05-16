347ae53fc204fd4f3b795abebb9196909c7dce31
wolfy1339 <webmaster@wolfy1339.com>
2019-05-15 23:17:22 -0400

    typescript: Convert App function to proper class

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,37 @@ import { request } from '@octokit/request'
 import { getCache } from './get-cache'
 import { getInstallationAccessToken } from './get-installation-access-token'
 import { getSignedJsonWebToken } from './get-signed-json-web-token'
+import LRUCache from 'lru-cache';
 
-export function App ({ id, privateKey, baseUrl, cache }) {
-  const state = {
-    id,
-    privateKey,
-    request: baseUrl ? request.defaults({ baseUrl }) : request,
-    cache: cache || getCache()
-  }
-  const api = {
-    getSignedJsonWebToken: getSignedJsonWebToken.bind(null, state),
-    getInstallationAccessToken: getInstallationAccessToken.bind(null, state)
-  }
-
-  return api
+interface AppOptions {
+  id: number
+  privateKey: string
+  baseUrl?: string
+  cache?: LRUCache<string, string>
 }
+
+export class App {
+  private state: {
+    id: number
+    privateKey: string
+    cache: LRUCache<string, string>
+    request: typeof request
+  };
+
+  constructor({ id, privateKey, baseUrl, cache }: AppOptions) {
+    this.state = {
+      id,
+      privateKey,
+      request: baseUrl ? request.defaults({ baseUrl }) : request,
+      cache: cache || getCache()
+    }
+  }
+  getSignedJsonWebToken(): string {
+    return getSignedJsonWebToken.bind(null, this.state)();
+  }
+
+  getInstallationAccessToken(options: { installationId: number }) {
+    return getInstallationAccessToken.bind(null, this.state)(options)
+  }
+}
+
