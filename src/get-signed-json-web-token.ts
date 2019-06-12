126ce4529f8c49a75e268f3d99bd98b32092b0a9
Michael Fix <mike@dawnlabs.io>
2019-06-12 13:12:41 -0700

    Update get-signed-json-web-token.ts

--- src/get-signed-json-web-token.ts
+++ src/get-signed-json-web-token.ts
@@ -9,7 +9,7 @@ export function getSignedJsonWebToken({ id, privateKey }: getSignedJWTOptions) {
   const now = Math.floor(Date.now() / 1000);
   const payload = {
     iat: now, // Issued at time
-    exp: now + 60 * 10, // JWT expiration time (10 minute maximum)
+    exp: now + 60 * 10 - 30, // JWT expiration time (10 minute maximum, 30 seconds of leeway)
     iss: id
   };
   const token = jsonwebtoken.sign(payload, privateKey, { algorithm: "RS256" });
